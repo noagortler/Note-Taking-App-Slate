8ba50ad2b95c01665f3cf9502d700b2938c262fc
noagortler <noagortler@gmail.com>
2026-03-17 13:53:16 -0700

    set up express and connect to MongoDB

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// temporary test route
+app.get("/", (req, res) => {
+  res.send("Slate is running!");
+});
+
+// connect to MongoDB then start the server
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Slate is running on http://localhost:${PORT}`);
+    });
+  })
+
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
